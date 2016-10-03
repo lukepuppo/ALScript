d2d341348af4e6c1a390d7fd3e5e21f67fe2b1d1
lukepuppo <lukepuppo@gmail.com>
2016-10-02 17:50:47 -0700

    Working 10/2 5:50
    Opens directory, and loads NVAX to the trade bar

--- program.py
+++ program.py
@@ -1,20 +1,15 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\Users\Luke Puppo\ALScript\ALScript\ChromeDriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Chrome('/Users/Luke Puppo/ALScript/ALScript/ChromeDriver')  # Optional argument, if not specified will search path.
 driver.get('http://www.marketwatch.com/game/');
 
-for x in range(0,10):
+for x in range(0,15):
 	time.sleep(1)
-	print ("Time left to login: " + str(10-x))
+	print ("Time left to login: " + str(15-x))
 	
-driver.get('http://www.marketwatch.com/game/wville-ap-econ/trade')
-
-time.sleep(3)
-search_box = driver.find_element_by_name('instrumentsearch ac_input unused')
-time.sleep(.1)
-search_box.equals(driver.switchTo().activeElement());
-search_box.send_keys('NVAX') #INSERT STOCK TO TRADE HERE
-search_box.submit()
+driver.get('http://www.marketwatch.com/game/wville-ap-econ/trade?view=detail&week=1&search=NVAX')
+time.sleep(1)
+driver.find_element_by_xpath("//*[@id='fakemaincontent']/section/div[2]/div/div[3]/div[2]/header/div[5]/button[2]").click()
 
 
